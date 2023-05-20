e0babeb5c28391f60e9ee09308b19c070d6da57e
Lalman <chauhanlakshay074@gmail.com>
2023-05-21 00:59:48 +0530

    feat: server init

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+
+const Port = process.env.PORT || 4000
+
+const app = express()
+
+const server = app.listen(Port, () => {
+    console.log(`Server is running on port ${Port}`)
+    }
+)
+
+const signals = ['SIGINT', 'SIGTERM']
+
+const gracefulleShutdown = (signal: string) => {
+    process.on(signal, async () => {
+        server.close()
+        console.log(`Server is shutting down due to ${signal}`)
+        
+        // disconnect from database
+        process.exit(0)
+    })
+}
+
+for (const signal of signals) {
+    gracefulleShutdown(signal)
+}
